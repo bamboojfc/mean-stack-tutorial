f215bf851458da9bcc219b340546397e9315b2aa
Banluesombatkul <U6063152@ten.thomsonreuters.com>
2017-06-21 11:56:30 +0700

    step2-products-success-to-read

---
+++ app/controllers/products.server.controller.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+	Product = mongoose.model('Product'),
+    _ = require('lodash');
+
+/**
+ * Create a Product
+ */
+exports.create = function(req, res) {
+    var product = new Product(req.body);
+
+    product.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.status(201).json(product);
+		}
+	});
+};
+
+/**
+ * Show the current Product
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Product
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Product
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Products
+ */
+exports.list = function(req, res) {
+	Product.find().sort('name').exec(function(err, products) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(products);
+		}
+	});
+};
---
+++ app/routes/products.server.routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(app) {
+	var products = require('../../app/controllers/products.server.controller');
+	
+	app.route('/products')
+		.get(products.list)
+		.post(products.create);
+};
